295636110608dc7fcecc02a8a3c56eb32d8b5073
theValiance <spiritofvale@gmail.com>
2018-01-26 19:40:05 -0500

    Cleaned up directories. Small changes to wording of documentation in permission.js. Added requirement lines to kuri2.js for permission.js and command.js.

--- kuri2.js
+++ kuri2.js
@@ -7,5 +7,12 @@ Created on 1/25/2018
 Main script for kuri2 project
 */
 
+const __script_directory = './kuri2/';
+const __plugin_directory = './plugins/';
+const __data_directory = './data/';
+
 //requirements
 const Discord = require("discord.js");
+const Command = require(`${__script_directory}command.js`);
+const Permission = require(`${__script_directory}permission.js`);
+
--- kuri2/permission.js
+++ kuri2/permission.js
@@ -7,10 +7,7 @@ File created on 1/25/2018
 Stores a constant for each permission bit, as well as some additional custom permission bits
 */
 
-//Permissions required for commands
-//Unused bits have been labeled and commented out, and are reserved for possible official permissions in the future
-//Custom permissions are listed further down
-//All 53 bits of the mantissa are used
+//constants for the permissions as specified in the discord documentation, as well as some constants for special permissions
 const permission = {
 //       BUILTIN PERMISSIONS       //
 CREATE_INSTANT_INVITE: 0x00000001,
@@ -21,8 +18,8 @@ MANAGE_CHANNELS: 0x00000010,
 MANAGE_GUILD: 0x00000020,
 ADD_REACTIONS: 0x00000040,
 VIEW_AUDIT_LOG: 0x00000080,
-//PERMISSION: 0x00000100, //unused bit
-//PERMISSION 0x00000200, //unused bit
+//PERMISSION: 0x00000100, //unused bit, presumed reserved
+//PERMISSION 0x00000200, //unused bit, presumed reserved
 VIEW_CHANNEL: 0x00000400,
 SEND_MESSAGES: 0x00000800,
 SEND_TTS_MESSAGES: 0x00001000,
@@ -32,7 +29,7 @@ ATTACH_FILES: 0x00008000,
 READ_MESSAGE_HISTORY: 0x00010000,
 MENTION_EVERYONE: 0x00020000,
 USE_EXTERNAL_EMOJIS: 0x00040000,
-//PERMISSION: 0x00080000, //unused bit
+//PERMISSION: 0x00080000, //unused bit, presumed reserved
 CONNECT: 0x00100000,
 SPEAK: 0x00200000,
 MUTE_MEMBERS: 0x00400000,
@@ -44,15 +41,15 @@ MANAGE_NICKNAMES: 0x08000000,
 MANAGE_ROLES: 0x10000000,
 MANAGE_WEBHOOKS: 0x20000000,
 MANAGE_EMOJIS: 0x40000000,
-//PERMISSION: 0x80000000, //unused bit
-//PERMISSION: 0x100000000, //unused bit
-//PERMISSION: 0x200000000, //unused bit
-//PERMISSION: 0x400000000, //unused bit
-//PERMISSION: 0x800000000, //unused bit
-//PERMISSION: 0x1000000000, //unused bit
-//PERMISSION: 0x2000000000, //unused bit
-//PERMISSION: 0x4000000000, //unused bit
-//PERMISSION: 0x8000000000, //unused bit
+//PERMISSION: 0x80000000, //unused bit, presumed reserved
+//PERMISSION: 0x100000000, //unused bit, presumed reserved
+//PERMISSION: 0x200000000, //unused bit, presumed reserved
+//PERMISSION: 0x400000000, //unused bit, presumed reserved
+//PERMISSION: 0x800000000, //unused bit, presumed reserved
+//PERMISSION: 0x1000000000, //unused bit, presumed reserved
+//PERMISSION: 0x2000000000, //unused bit, presumed reserved
+//PERMISSION: 0x4000000000, //unused bit, presumed reserved
+//PERMISSION: 0x8000000000, //unused bit, presumed reserved
 //         CUSTOM PERMISSIONS         //
 CUSTOM_1: 0x10000000000,
 CUSTOM_2: 0x20000000000,
@@ -70,5 +67,5 @@ BOT_OWNER: 0x10000000000000 //only the user running/hosting the bot can use comm
 //ANY HIGHER WILL RESULT IN AN INTEGER OVERFLOW//
 };
 
-//make the variables directly accessible to the importer
+//make the constants directly accessible to the importer
 module.exports = permission;
